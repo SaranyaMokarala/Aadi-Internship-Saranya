7875ed3cc22105a333ec05edc0d52f6972338ed3
Saranya <mslv.saranya@gmail.com>
2024-06-02 23:21:09 +0530

    REST API Project done

--- REST API Project/scripts.js
+++ REST API Project/scripts.js
@@ -11,10 +11,12 @@ async function getdata()
     const allposts = document.getElementById("allposts");
     allposts.innerHTML = "";
     postdata.forEach(post => {
+        const postendpoint = "https://jsonplaceholder.typicode.com/posts";
         const postdiv = document.createElement('div');
         postdiv.id = "post"+String(post.id)
         postdiv.className = "post";
         postdiv.innerHTML = "<p class='title'>"+String(post.title)+"</p>"+
+        "<p class='postbody' id='postbody"+String(post.id)+"'>"+String(post.body)+"</p>"+
         "<p class='name'>"+String(userdata[post.userId-1].name)+"</p>"+
         "<p class='email'>"+String(userdata[post.userId-1].email)+"</p>"+
         '<button onclick="showpost('+post.id+')">Details</button>'
@@ -25,10 +27,22 @@ async function getdata()
 
 async function showpost(postId)
 {
-    const commentendpoint = "https://jsonplaceholder.typicode.com/comments?postId=<"+String(postId)+">";
+    const commentendpoint = "https://jsonplaceholder.typicode.com/comments?postId="+String(postId);
     let commentresponse = await fetch(commentendpoint);
-    let commentdata = await commentresponse.json();  
-    
-    const post = document.getElementById("post"+String(postId));
+    let commentdata = await commentresponse.json();
     console.log(commentdata);
+
+    const postbody = document.getElementById("postbody"+postId);
+    postbody.style.display = 'block';
+
+    const post = document.getElementById("post"+postId);
+    
+    commentdata.forEach(comment => {
+        const commentdiv = document.createElement('div');
+        commentdiv.className = "comment";
+        commentdiv.innerHTML = "<p class='cname'>"+comment.name+"</p>"+
+        "<p class='cbody'>"+comment.body+"</p>";
+        post.appendChild(commentdiv);
+    })
+    
 }
